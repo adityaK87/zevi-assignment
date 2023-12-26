793c1014a60b31dc5d10c3002cc9fc4c01aa0125
Aditya Kumar <adityapal1409@gmail.com>
2023-12-26 23:42:29 +0530

    create button to add to wishlist

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.scss";
+import { CiHeart } from "react-icons/ci";
+import { FaHeart } from "react-icons/fa";
 
 const ProductCard = () => {
+	const [isAddedToWishlist, setIsAddedToWishList] = useState(true);
+
 	return (
 		<div className="productCard">
-			<img
-				src="https://www.feltright.in/cdn/shop/files/WhatsAppImage2023-08-09at5.02.23PM_2.jpg?v=1691580853&width=1000"
-				alt="Product Name"
-				className="productImage"
-			/>
+			<div className="imageContainer">
+				<img
+					src="https://www.feltright.in/cdn/shop/files/WhatsAppImage2023-08-09at5.02.23PM_2.jpg?v=1691580853&width=1000"
+					alt="Product Name"
+					className="productImage"
+				/>
+
+				{/* Add to wishlist  */}
+				{isAddedToWishlist ? (
+					<span
+						className="wishlistButton"
+						onClick={() =>
+							setIsAddedToWishList(!isAddedToWishlist)
+						}>
+						<CiHeart />
+					</span>
+				) : (
+					<span
+						className="filled"
+						onClick={() =>
+							setIsAddedToWishList(!isAddedToWishlist)
+						}>
+						<FaHeart />
+					</span>
+				)}
+			</div>
 			<p className="productName">Flop turn ruler</p>
 			<p>
 				<span className="productMRP">Rs.549</span>

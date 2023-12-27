9f7e0ab30ad2164673ea3126353a9670087c1be0
Aditya Kumar <adityapal1409@gmail.com>
2023-12-27 14:09:27 +0530

    modify app component to show how  UI is looking

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import SearchBox from "./components/SearchBox";
+import "./App.scss";
+import ProductList from "./components/ProductList";
+import FilterComponent from "./components/FilterComponent";
 
 const App = () => {
-	return <div>Initialize the Project</div>;
+	return (
+		<section className="homeElement">
+			<SearchBox />
+			<p className="searchResults">Search Results</p>
+			<section className="sectionContainer">
+				<FilterComponent />
+
+				<ProductList />
+			</section>
+		</section>
+	);
 };
 
 export default App;

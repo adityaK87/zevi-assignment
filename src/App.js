57bddd616aad3634833adca391b3f403ace1e09e
Aditya Kumar <adityapal1409@gmail.com>
2023-12-27 23:42:12 +0530

    fetching data from api

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
-import React from "react";
+import React, { useState, useReducer, useEffect } from "react";
 import SearchBox from "./components/SearchBox";
 import "./App.scss";
 import ProductList from "./components/ProductList";
 import FilterComponent from "./components/FilterComponent";
+import LatestTrend from "./components/LatestTrend";
+import { getData } from "./api";
+import { reducer } from "./reducer";
+
+const initialState = {
+	productData: [],
+	loading: false,
+	errMessage: null,
+};
 
 const App = () => {
+	const [state, dispatch] = useReducer(reducer, initialState);
+	const [isInsideSearch, setIsInsideSearch] = useState(false);
+
+	useEffect(() => {
+		getData(dispatch);
+	}, []);
+
+	console.log("state.products", state.productData);
 	return (
 		<section className="homeElement">
-			<SearchBox />
-			<p className="searchResults">Search Results</p>
-			<section className="sectionContainer">
-				<FilterComponent />
+			<SearchBox setIsInsideSearch={setIsInsideSearch} />
 
-				<ProductList />
-			</section>
+			{isInsideSearch ? (
+				<LatestTrend />
+			) : (
+				<>
+					<p className="searchResults">Search Results</p>
+					<section className="sectionContainer">
+						<FilterComponent />
+
+						{state.productData.length > 0 ? (
+							<ProductList productData={state.productData} />
+						) : (
+							<p>Loading...</p>
+						)}
+					</section>
+				</>
+			)}
 		</section>
 	);
 };
---
+++ src/api/index.js
@@ -0,0 +1,14 @@
+export const getData = async (dispatch) => {
+	dispatch({ type: "START_FETCHING_DATA" });
+	try {
+		fetch("https://fakestoreapi.com/products")
+			.then((res) => res.json())
+			.then((data) => {
+				console.log("data", data);
+				dispatch({ type: "GET_DATA", payload: data });
+			});
+	} catch (err) {
+		dispatch({ type: "FETCH_FAILURE", payload: err.message });
+		console.log(err.message);
+	}
+};
